77d7f894d5be931faad3403949d69e590b38b990
fperez <frvpillatoro@gmail.com>
2018-10-11 16:13:59 -0500

    Adding module for generating bootstrap positions from bim files, and calculates Fst values in those positions

---
+++ analysis/001b-BoostrapFst/bin/random_get.R
@@ -0,0 +1,21 @@
+##### Getting arguments ####
+args = commandArgs(trailingOnly=TRUE)
+input = args[1]
+output = args[2]
+size = args[3]
+positions = args[4]
+
+
+#Defining the input
+vars <- read.csv(file=input, sep="\t", header=F)
+
+#Getting the random numbers to take from the input
+randi <- sort(sample(c(1:size), positions, replace = TRUE))
+
+
+#Taking the input rows from the input
+set <- vars[randi,]
+
+#Writing the output to a file
+write.table(set,file=output,quote=F,sep="\t",row.names=F,col.names=F)
+
